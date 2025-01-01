036f28fd02212b6d70ed368c30c091b9634c86af
Daniel Hurtado <a01707774@tec.mx>
2025-01-01 21:36:21 +0100

    feat(activity, communities): add Activity and Communities pages with basic structure

---
+++ app/(root)/activity/page.tsx
@@ -0,0 +1,5 @@
+function Activity() {
+  return <h1 className="head-text">Activity</h1>;
+}
+
+export default Activity;
---
+++ app/(root)/communities/page.tsx
@@ -0,0 +1,5 @@
+function Communities() {
+  return <h1 className="head-text">Communities</h1>;
+}
+
+export default Communities;
