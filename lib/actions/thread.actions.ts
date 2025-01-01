dbf810e46eb4598dcaef3e0c781413126fc75ddd
Daniel Hurtado <a01707774@tec.mx>
2025-01-01 21:19:24 +0100

    fix(thread.actions): streamline population of author and children fields in fetchPosts function for improved clarity and maintainability

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -28,18 +28,18 @@ export async function fetchPosts(pageNumber = 1, pageSize = 20) {
       model: Community,
     })
     .populate({
-      path: "children", // Populate the children field
+      path: "children",
       populate: {
-        path: "author", // Populate the author field within children
+        path: "author",
         model: User,
-        select: "_id name parentId image", // Select only _id and username fields of the author
+        select: "_id name parentId image",
       },
     });
 
   // Count the total number of top-level posts (threads) i.e., threads that are not comments.
   const totalPostsCount = await Thread.countDocuments({
     parentId: { $in: [null, undefined] },
-  }); // Get the total count of posts
+  });
 
   const posts = await postsQuery.exec();
 
